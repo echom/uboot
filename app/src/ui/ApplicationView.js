2dbccda4ddd11e4eadcdfdef786b0dc18a3cbef2
Jörg Hösel <echom@gmx.net>
2016-06-29 09:06:57 +0200

    progress on views

--- app/src/ui/ApplicationView.js
+++ app/src/ui/ApplicationView.js
@@ -1,14 +1,15 @@
 np.define('ui.ApplicationView', function() {
   var DomContainer = np.require('ui.DomContainer'),
       Toolbar = np.require('ui.Toolbar'),
-      // ProjectView = np.require('ui.ProjectView'),
+      ProjectView = np.require('ui.ProjectView'),
       ApplicationView;
 
   ApplicationView = np.inherits(function(application) {
     DomContainer.call(this, 'div');
     this._application = application;
     this._toolbar = this.append(new Toolbar(application));
-    // this._projectView = this.append(new ProjectView(application.project));
+    this._projectView = this.append(new ProjectView(application.project));
+    
     this.addClass('app');
   }, DomContainer);
 
--- app/src/ui/ProjectView.js
+++ app/src/ui/ProjectView.js
@@ -1,10 +1,16 @@
 np.define('ui.ProjectView', function() {
-  var DomContainer = np.require('np.DomContainer'),
-      ScenesView = np.require('ui.ScenesView'),
+  var DomContainer = np.require('ui.DomContainer'),
+      // ScenesView = np.require('ui.ScenesView'),
+      // RenderView = np.require('ui.RenderView'),
       ProjectView;
 
   ProjectView = np.inherits(function(project) {
-    this.scenesView = this.append(new ScenesView(project.getScenes()));
-    this.renderView = this.append(new RenderView(project));
+    DomContainer.call(this, 'div');
+    // this._scenesView = this.append(new ScenesView(project.getScenes()));
+    // this._renderView = this.append(new RenderView(project));
+
+    this.addClass('app-project');
   }, DomContainer);
+
+  return ProjectView;
 });
