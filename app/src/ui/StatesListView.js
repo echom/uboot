8d49205d5438f6a452bcdcd7075030551343a041
Jörg Hösel <echom@gmx.net>
2016-08-10 16:50:10 +0200

    duration label fix

--- app/src/ui/StatesListView.js
+++ app/src/ui/StatesListView.js
@@ -1,9 +1,7 @@
 np.define('ui.StatesListView', () => {
   var Element = np.require('ui.Element'),
-      Button = np.require('ui.Button'),
       List = np.require('ui.List'),
-      Icon = np.require('ui.Icon'),
-      State = np.require('model.State');
+      Icon = np.require('ui.Icon');
 
   class StatesListItem extends List.Item {
     constructor(state, player) {
@@ -18,13 +16,41 @@ np.define('ui.StatesListView', () => {
       this._duration.setContent(this._state.getDuration() || '');
       this._state.onDurationChanged((evt) => {
         this._indicator.setContent(evt.newValue <= 0 ? Icon.str('mouse') : Icon.str('play_arrow'));
-        this._duration.setContent(evt.newValue || '');
+        this._setDuration(evt.newValue);
       });
 
       this.toggleClass('current', player.getState() === state);
       player.onStateChanged((evt) => {
         this.toggleClass('current', evt.newValue === state);
       });
+    }
+
+    _setDuration(duration, force) {
+      var content = [],
+          d = (isFinite(duration) && duration > 0) ? duration : 0,
+          h, m, s, ms;
+
+      h = Math.floor(d / 360000);
+      d %= 360000;
+      m = Math.floor(d / 60000);
+      d %= 60000;
+      s = Math.floor(d / 1000);
+      d %= 1000;
+      ms = d;
+
+      if (h) {
+        content = [h + 'h', m + 'm', s + 's', ms + 'ms'].join(':');
+      } else if (m) {
+        content = [m + 'm', s + 's', ms + 'ms'].join(':');
+      } else if (s) {
+        content = [s + 's', ms + 'ms'].join(':');
+      } else if (ms) {
+        content = ms + 'ms';
+      } else {
+        content = '';
+      }
+
+      this._duration.setContent(content, force);
     }
   }
 
